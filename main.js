2f723b83f99365b0aee13a3b1add9acbdafa7a24
Emilia Zysk <emzysk.dev@gmail.com>
2023-11-20 19:23:52 +0900

    animate

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import "./style.css";
 import * as THREE from "three";
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color("#ADD8E6");
+// scene.background = new THREE.Color("#ADD8E6");
 
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -17,8 +17,25 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
+// camera.position.set(0, 2, 30);
 camera.position.setZ(30);
+// camera.position.setX(-3);
 
 renderer.render(scene, camera);
 
-// const geometry = new
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
+
+const animate = () => {
+  requestAnimationFrame(animate);
+
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+
+  renderer.render(scene, camera);
+};
+animate();
