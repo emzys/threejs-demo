6f4f6774f2b029c119b9444e4ba7b56b6e08eb95
Emilia Zysk <emzysk.dev@gmail.com>
2023-11-20 19:07:23 +0900

    bugfix

--- main.js
+++ main.js
@@ -6,13 +6,12 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
-  0,
-  1,
+  0.1,
   1000
 );
 
 const renderer = new THREE.WebGLRenderer({
-  cnavas: document.querySelector("#bg"),
+  canvas: document.querySelector("#bg"),
 });
 
 renderer.setPixelRatio(window.devicePixelRatio);
